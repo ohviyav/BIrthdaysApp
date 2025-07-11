69304a9e02cbf9e355811b962f559240b4ba8eea
Ohviya Velu <ohviya.pugal@gmail.com>
2025-07-11 09:32:57 -0500

    Initial Commit

---
+++ BIrthdaysApp/BIrthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIrthdaysAppApp.swift
+//  BIrthdaysApp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BIrthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
