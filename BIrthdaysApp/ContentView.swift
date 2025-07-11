6fa11830600a13d78ebe51b03b1af40c185a2cce
Ohviya Velu <ohviya.pugal@gmail.com>
2025-07-11 10:35:20 -0500

    We created a birthday app

--- BIrthdaysApp/ContentView.swift
+++ BIrthdaysApp/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "John", birthday: .now),
+        Friend(name: "Samantha", birthday: Date(timeIntervalSince1970: 0)) ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+        //id tells the computer that each string taken in belongs to one person
+        //friend in builds a row as each person. stacks on top of eachother
+        List(friends, id: \.name) { friend in
+            HStack {
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            }
+            
         }
-        .padding()
+        .navigationTitle("my awesome bday app")
+        .safeAreaInset(edge: .bottom) {
+            VStack(alignment: .center, spacing: 20) {
+                Text("New Birthday")
+                    .font(.headline)
+                DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now,displayedComponents:.date) {
+                    //textFieldStyle adds a box around the text input section
+                    TextField( "Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                    
+                }
+                Button("Save Birthday") {
+                    let newFriend = Friend(name: newName, birthday: newBirthday)
+                    friends.append(newFriend)
+                    newName = ""
+                    newBirthday = .now
+                }
+                .padding()
+                .background(.bar)
+                
+            }
+        }
+        
+        }
     }
+    
 }
 
 #Preview {
---
+++ BIrthdaysApp/Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  BIrthdaysApp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
